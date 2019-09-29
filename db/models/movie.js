1935626faa3601477810d6b7ddab24b8154d7fb4
flamerged <mersad.ajanovic@googlemail.com>
2019-09-29 19:36:02 +0200

    Expanded the Movie model with more data column properties and added validation

--- db/models/movie.js
+++ db/models/movie.js
@@ -4,7 +4,59 @@ module.exports = sequelize => {
     class Movie extends Sequelize.Model {}
     Movie.init(
         {
-            title: Sequelize.STRING
+            id: {
+                type: Sequelize.INTEGER,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            title: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                validate: {
+                    notNull: {
+                        msg: 'Please provide a value for "title"',
+                      },
+                    notEmpty: {
+                      // custom error message
+                      msg: 'Please provide a value for "title"',
+                    }
+            },
+            runtime: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                validate: {
+                    notNull: {
+                        msg: 'Please provide a value for "title"',
+                      },
+                      min: { 
+                        args: 1,
+                        msg: 'Please provide a value greater than "0" for "runtime"',
+                    },
+                }
+            },
+            releaseDate: {
+                type: Sequelize.DATEONLY,
+                allowNull: false,
+                validate: {
+                    notNull: {
+                        msg: 'Please provide a value for "title"',
+                      },
+                      isAfter: {
+                        args: '1895-12-27',
+                        msg: 'Please provide a value on or after "1895-12-28" for "releaseDate"',
+                      },
+                }
+            },
+            isAvailableOnVHS: {
+                type: Sequelize.BOOLEAN,
+                allowNull: false,
+                defaultValue: false,
+                validate: {
+                    notNull: {
+                        msg: 'Please provide a value for "title"',
+                      },
+                }
+            }
         },
         { sequelize }
     );
